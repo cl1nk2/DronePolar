a9ee704aa232061db8f5b57c448b1c9b30cb4cd9
cl1nk2 <smoothie@TPROGZephyrus>
2023-05-07 15:35:39 +0200

    bugfixes

--- CalculateWaterSurface/water.py
+++ CalculateWaterSurface/water.py
@@ -11,11 +11,20 @@ img_name_water = sys.argv[2]
 img_int = cv2.imread(img_name_int)
 img_water = cv2.imread(img_name_water)
 
+height1, width1, _ = img_int.shape
+
+img_water = cv2.resize(img_water, (width1, height1))
+
 int_mask = cv2.inRange(img_int, np.array([0, 0, 255]), np.array([0, 0, 255]))
 water_mask = cv2.inRange(img_water, np.array([255, 0, 0]), np.array([255, 0, 0]))
 
 output = cv2.bitwise_and(int_mask, water_mask)
 
-result = np.sum(output==255)/np.sum(int_mask==255)
+int_pixel_count = np.sum(int_mask==255)
+
+if int_pixel_count > 0:
+    result = np.sum(output==255)/int_pixel_count
+else:
+    result = 0
 
 print(result)
