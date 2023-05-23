36ce5aa9be2fb495331187d942a197f89b2b68f9
cl1nk2 <petertakacs92@gmail.com>
2023-05-23 14:05:47 +0200

    added over and underexposed thresholds to calculation, now requires original intensity image aswell

--- CalculateWaterSurface/water.py
+++ CalculateWaterSurface/water.py
@@ -6,17 +6,41 @@ import cv2
 import numpy as np
 
 img_name_int = sys.argv[1]
-img_name_water = sys.argv[2]
+img_name_mask = sys.argv[2]
+img_name_water = sys.argv[3]
 
 img_int = cv2.imread(img_name_int)
+img_mask = cv2.imread(img_name_mask)
 img_water = cv2.imread(img_name_water)
 
-height1, width1, _ = img_int.shape
+height, width, _ = img_int.shape
 
-img_water = cv2.resize(img_water, (width1, height1))
+img_water = cv2.resize(img_water, (width, height))
+img_mask = cv2.resize(img_mask, (width, height))
 
-int_mask = cv2.inRange(img_int, np.array([0, 0, 255]), np.array([0, 0, 255]))
+img_int_grey = cv2.cvtColor(img_int, cv2.COLOR_BGR2GRAY)
+
+overexposed_threshold = 230.0
+img_overexposed_mask = (img_int_grey >= overexposed_threshold).astype(np.uint8)*255
+
+underexposed_threshold = 5.0
+img_underexposed_mask = (img_int_grey <= underexposed_threshold).astype(np.uint8)*255
+
+img_overunderexposed_mask = cv2.bitwise_or(img_overexposed_mask, img_underexposed_mask)
+
+#cv2.imwrite('img_overexposed_mask.png', img_overexposed_mask)
+#cv2.imwrite('img_underexposed_mask.png', img_underexposed_mask)
+#cv2.imwrite('img_overunderexposed_mask.png', img_overunderexposed_mask)
+
+int_mask = cv2.inRange(img_mask, np.array([0, 0, 255]), np.array([0, 0, 255]))
 water_mask = cv2.inRange(img_water, np.array([255, 0, 0]), np.array([255, 0, 0]))
+
+#cv2.imwrite('int_mask.png', int_mask)
+#cv2.imwrite('water_mask.png', water_mask)
+
+int_mask = cv2.bitwise_and(int_mask, cv2.bitwise_not(img_overunderexposed_mask))
+
+#cv2.imwrite('int_mask_final.png', int_mask)
 
 output = cv2.bitwise_and(int_mask, water_mask)
 
